36bfbafc839887418bed0a66bbbc0d60067a2102
Varsha P <142588198+Varshap2205@users.noreply.github.com>
2024-08-05 19:58:06 +0530

    Add files via upload

---
+++ DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>VogueVista is a premier online fashion destination, offering an exquisite collection of the latest trends and timeless styles. With a curated selection of designer apparel, accessories, and footwear, VogueVista caters to fashion enthusiasts who seek elegance and sophistication. The platform features exclusive interviews with top designers, fashion tips from industry experts, and a vibrant community for style inspiration. VogueVista is not just a shopping site; it's a fashion experience that celebrates individuality and innovation, making it the go-to hub for fashion-forward individuals.</p>
+        <p>VogueVista offers the latest in designer apparel, accessories, and footwear, along with exclusive fashion insights and a vibrant style community, making it the ultimate destination for fashion enthusiasts.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
